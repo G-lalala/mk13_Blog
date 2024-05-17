80cf08fe26a256a5d1a08f586c49a1e6c6e20f8a
G-lalala <miyake.ba55@gmail.com>
2024-05-17 19:30:33 +0900

    add:テストが動かせるように環境整備

--- frontend/app/_functions/fetchPosts/fetchPosts.ts
+++ frontend/app/_functions/fetchPosts/fetchPosts.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-export async function fetchPosts() {
+export async function fetchPosts(): Promise<string[]> {
   const posts = fs.readdirSync('/app/app/_post')
   return posts
 }
